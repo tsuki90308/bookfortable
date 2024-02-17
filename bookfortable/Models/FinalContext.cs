d463e5ed1c1257a63e0b70352d8536c9d5250f6d
Ryder <realsam199510@gmail.com>
2024-02-17 11:11:19 +0800

    update EF

--- bookfortable/Models/FinalContext.cs
+++ bookfortable/Models/FinalContext.cs
@@ -51,7 +51,7 @@ public partial class FinalContext : DbContext
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Final;Integrated Security=True;Trust Server Certificate=True");
+//        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Final;Integrated Security=True;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -75,7 +75,9 @@ public partial class FinalContext : DbContext
 
             entity.ToTable("BookTag");
 
-            entity.Property(e => e.BtagName).HasMaxLength(50);
+            entity.Property(e => e.BtagName)
+                .IsRequired()
+                .HasMaxLength(50);
         });
 
         modelBuilder.Entity<DiscountCodeCart>(entity =>
@@ -315,7 +317,6 @@ public partial class FinalContext : DbContext
 
             entity.HasOne(d => d.BookTag).WithMany(p => p.Relations)
                 .HasForeignKey(d => d.BookTagId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Relation_BookTag");
 
             entity.HasOne(d => d.Product).WithMany(p => p.Relations)
