88624fc771dde2ad9698125b6824a4a1345d2d37
ArielChen <octopusacid@gmail.com>
2024-02-27 10:10:14 +0800

    20240227-修正問號 & 新增購物車控制器(尚未完成)

--- bookfortable/Controllers/ShoppingController.cs
+++ bookfortable/Controllers/ShoppingController.cs
@@ -1,4 +1,5 @@
 ﻿using Bookfortable.Models;
+using bookfortable.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
@@ -7,79 +8,12 @@ namespace Bookfortable.Controllers
 {
     public class ShoppingController : Controller
     {
-        // GET: ShoppingController
-        public ActionResult Index()
+        //虛擬商場, 之後連接組長負責的部分
+        public IActionResult List()
         {
-            return View();
-        }
-
-        // GET: ShoppingController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ShoppingController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ShoppingController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ShoppingController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ShoppingController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ShoppingController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ShoppingController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            FinalContext db = new FinalContext();
+            var datas = from p in db.Members select p;
+            return View(datas);
         }
 
         //購物車頁首
