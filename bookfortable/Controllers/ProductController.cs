1a4d9313016c6a2715501c4300bae3c569409e10
Huang Chi-Han <chichi22.hh@gmail.com>
2024-03-07 14:38:05 +0800

    0307提交(layout 書籍管理)

--- bookfortable/Controllers/ProductController.cs
+++ bookfortable/Controllers/ProductController.cs
@@ -1,34 +1,81 @@
 ﻿using bookfortable.Models;
 using bookfortable.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
+using X.PagedList;
+
 
 namespace bookfortable.Controllers
 {
     public class ProductController : Controller
     {
+
+
+
         private IWebHostEnvironment _enviro = null;
         public ProductController(IWebHostEnvironment p)
         {
             _enviro = p;
         }
-        public IActionResult List(PKeywordVewModel vm)
+
+        //public IActionResult List(PKeywordVewModel vm)
+        //{
+        //    FinalContext db = new FinalContext();
+        //    IEnumerable<Product> datas = null;
+        //    if (string.IsNullOrEmpty(vm.txtKeyword))
+        //        datas = from p in db.Products
+        //                select p;
+        //    else
+        //        datas = db.Products.Where(p =>
+        //            p.ProductName.Contains(vm.txtKeyword) ||
+        //            p.SupplierId.Contains(vm.txtKeyword) ||
+        //            p.ProductPhoto.Contains(vm.txtKeyword) ||
+        //            p.Description.Contains(vm.txtKeyword) ||
+        //            p.Isbn.ToString().Contains(vm.txtKeyword)
+        //        );
+        //    return View(datas);
+        //}
+
+        public IActionResult List(PKeywordVewModel vm, int? page)
         {
             FinalContext db = new FinalContext();
-            IEnumerable<Product> datas = null;
-            if (string.IsNullOrEmpty(vm.txtKeyword))
-                datas = from p in db.Products
-                        select p;
-            else
-                datas = db.Products.Where(p =>
+            IQueryable<Product> datas = db.Products; // 使用 IQueryable 以支援分頁
+
+            // 如果有關鍵字，進行過濾
+            if (!string.IsNullOrEmpty(vm.txtKeyword))
+            {
+                datas = datas.Where(p =>
                     p.ProductName.Contains(vm.txtKeyword) ||
                     p.SupplierId.Contains(vm.txtKeyword) ||
                     p.ProductPhoto.Contains(vm.txtKeyword) ||
                     p.Description.Contains(vm.txtKeyword) ||
                     p.Isbn.ToString().Contains(vm.txtKeyword)
                 );
-            return View(datas);
+            }
+
+            // 獲取符合條件的資料總數
+            int totalCount = datas.Count();
+
+            // 分頁
+            int pageSize = 10; // 每頁顯示的項目數
+            int pageNumber = (page ?? 1); // 如果 page 為空，預設為第 1 頁
+
+            // 將資料集合轉換為分頁後的集合
+            var pagedData = datas.ToPagedList(pageNumber, pageSize);
+
+            // 將總筆數傳遞給視圖
+            ViewBag.TotalCount = totalCount;
+
+            // 獲取資料庫中的全部資料總數
+            int totalRecordsCount = db.Products.Count();
+
+            // 將全部資料總數傳遞給視圖
+            ViewBag.TotalRecordsCount = totalRecordsCount;
+
+            return View(pagedData);
         }
+
         [HttpGet]
         public IActionResult ClearSearch()
         {
@@ -70,16 +117,41 @@ namespace bookfortable.Controllers
         //    return RedirectToAction("List");
         //}
 
+        //[HttpPost]
+        //public IActionResult Create(CProductWrap p)
+        //{
+        //    FinalContext db = new FinalContext();
+        //    if (p.photo != null)
+        //    {
+        //        string photoName = Guid.NewGuid().ToString() + ".jpg";
+        //        p.product.ProductPhoto = photoName;
+        //        p.photo.CopyTo(new FileStream(_enviro.WebRootPath + "images" + photoName, FileMode.Create));
+        //    }
+        //    db.Products.Add(p.product);
+        //    db.SaveChanges();
+        //    return RedirectToAction("List");
+        //}
+
+
         [HttpPost]
         public IActionResult Create(CProductWrap p)
         {
             FinalContext db = new FinalContext();
             if (p.photo != null)
             {
-                string photoName = Guid.NewGuid().ToString() + ".jpg";
+                // 生成新的文件名，基于当前时间戳
+                string photoName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".jpg";
+
+                // 保存图像到文件系统中
+                using (FileStream fs = new FileStream(Path.Combine(_enviro.WebRootPath, "images", photoName), FileMode.Create))
+                {
+                    p.photo.CopyTo(fs);
+                }
+
+                // 更新产品的图像信息为文件名
                 p.product.ProductPhoto = photoName;
-                p.photo.CopyTo(new FileStream(_enviro.WebRootPath + "images" + photoName, FileMode.Create));
             }
+
             db.Products.Add(p.product);
             db.SaveChanges();
             return RedirectToAction("List");
@@ -107,15 +179,21 @@ namespace bookfortable.Controllers
             {
                 if (pIn.photo != null)
                 {
-                    string photoName = Guid.NewGuid().ToString() + ".jpg";
+                    // 生成新的文件名，基于当前时间戳
+                    string photoName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".jpg";
                     pEdit.ProductPhoto = photoName;
-                    pIn.photo.CopyTo(new FileStream(_enviro.WebRootPath + "images" +photoName, FileMode.Create));
+
+                    // 保存图像到文件系统中
+                    using (FileStream fs = new FileStream(Path.Combine(_enviro.WebRootPath, "images", photoName), FileMode.Create))
+                    {
+                        pIn.photo.CopyTo(fs);
+                    }
                 }
 
+                // 更新其他产品信息
                 pEdit.ProductId = pIn.ProductId;
                 pEdit.ProductName = pIn.ProductName;
                 pEdit.SupplierId = pIn.SupplierId;
-                //pEdit.ProductPhoto = pIn.ProductPhoto;
                 pEdit.Description = pIn.Description;
                 pEdit.UnitPrice = pIn.UnitPrice;
                 pEdit.UnitsInStock = pIn.UnitsInStock;
@@ -128,5 +206,7 @@ namespace bookfortable.Controllers
             }
             return RedirectToAction("List");
         }
+
+
     }
 }
---
+++ bookfortable/Models/DTO/SpotsPagingDTO.cs
@@ -0,0 +1,11 @@
+﻿using bookfortable.Models;
+
+namespace bookfortable.Models.DTO
+{
+    public class SpotsPagingDTO
+    {
+        public int TotalPages { get; set; }
+       // public int TotalCount { get; set; }
+       // public List<SpotImagesSpot>? SpotsResult { get; set; }
+    }
+}
