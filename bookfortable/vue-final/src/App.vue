1fc52fd59a668133ac87dbcfdf2013731ab03cbe
Ryder <realsam199510@gmail.com>
2024-03-13 20:34:57 +0800

    前台連結

--- bookfortable/vue-final/src/App.vue
+++ bookfortable/vue-final/src/App.vue
@@ -111,42 +111,30 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-3">
                         <div class="header__logo">
-                            <a asp-controller="Home" asp-action="Test"><img src="./img/logo.png" alt=""></a>
+                            <a href="https://localhost:7174/" asp-action="Test"><img src="./img/logo.png" alt=""></a>
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-6">
                         <nav class="header__menu mobile-menu">
                             <ul>
-
-                                <li class="active"><a href="/" asp-controller="Quiz" asp-action="index">測驗</a></li>
-                                <li><a href="./shop.html">Shop</a></li>
-                                <li>
-                                    <a href="#">Pages</a>
-                                    <ul class="dropdown">
-                                        <li><a href="./about.html">About Us</a></li>
-                                        <li><a href="./shop-details.html">Shop Details</a></li>
-                                        <li><a href="./shopping-cart.html">Shopping Cart</a></li>
-                                        <li><a href="./checkout.html">Check Out</a></li>
-                                        <li><a href="./blog-details.html">Blog Details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="./blog.html">Blog</a></li>
-                                <li><a href="/contact">Contacts</a></li>
-                                <li><a href="/wishlist">WishList</a></li>                               
+                                <li class="active"><a href="https://localhost:7174/Quiz/index" asp-controller="Quiz" asp-action="index">測驗</a></li>
+                                <li><a href="https://localhost:7174/Eventfront/List">活動</a></li>
+                                <li><a href="/wishlist">願望清單</a></li>
+                                <li><a href="/wishlist">聊天室</a></li>
+                                <li><a href="https://localhost:7174/Shopping/List" src="./img/icon/search.png" alt="">購物車</a></li>
+                            
                             </ul>
-                        </nav>
-                    </div>
-                    <div class="col-lg-3 col-md-3">
-                        <div class="header__nav__option">
-                            <a href="#" class="search-switch"><img src="./img/icon/search.png" alt=""></a>
                             <a href="#"><img src="./img/icon/heart.png" alt=""></a>
                             <a href="#"><img src="./img/icon/cart.png" alt=""> <span>0</span></a>
                             <div class="price">$0.00</div>
+                            
+                            </nav>
                         </div>
                     </div>
                 </div>
+            
                 <div class="canvas__open"><i class="fa fa-bars"></i></div>
-            </div>
+            
         </header>
         <!-- Header Section End -->
 
